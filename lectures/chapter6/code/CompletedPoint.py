fa95dcb1812b08787668ec70939ffe6568da9903
Lukas Bystricky <lb13f@my.fsu.edu>
2017-07-06 09:34:44 -0400

    CompletedPoint.py: initial commit

---
+++ lectures/chapter6/code/CompletedPoint.py
@@ -0,0 +1,88 @@
+class Point:
+    """ 
+    A class that represents a point (x,y) in a 2D plane. 
+    """
+    
+    def __init__(self, x=0, y=0):
+        """ 
+        Constructor
+        
+        Parameters
+        ----------
+        x [float]
+        y [float]
+        """
+        
+        self._x = x
+        self._y = y
+        
+    def getX(self):
+        """
+        Get x
+        """
+        return self._x
+    
+    def getY(self):
+        """
+        Get y
+        """
+        return self._y
+    
+    def setX(self,value):
+        """
+        Set x
+        
+        Parameters
+        ----------
+        x [float]
+        """
+        self._x = value
+        
+    def setY(self, value):
+        """
+        Set y
+        
+        Parameters
+        ----------
+        y [float]
+        """
+        self._y = value
+        
+    def scale(self, factor):
+        """ Scales both coordinate values by a factor
+        
+        Parameters
+        ----------
+        factor [float]
+        """
+        self._x *= factor
+        self._y *= factor
+    
+    def distance(self, other):
+        """
+        Computes the distance between this point and another point
+        
+        Parameters
+        ----------
+        other [Point] 
+        
+        Returns
+        -------
+        d [float] : distance between this point and other point
+        """
+        
+        import numpy as np
+        dx = self._x - other._x
+        dy = self._y - other._y
+    
+        return np.sqrt(dx**2 + dy**2)
+
+def normalize(self):
+    """
+    Scales the point to have unit length from origin
+    """
+    
+    mag = self.distance(Point()) # compute the distance from point to the origin
+    if mag > 0:
+        # if the magnitude is not 0, scale by 1 over magnitude
+        self.scale(1/mag)
