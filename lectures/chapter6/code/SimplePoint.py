a9f62c26d29a40b314f5648232f44c1083c99668
Lukas Bystricky <lb13f@my.fsu.edu>
2017-07-06 09:34:23 -0400

    SimplePoint.py: initial commit

---
+++ lectures/chapter6/code/SimplePoint.py
@@ -0,0 +1,16 @@
+class Point:
+    def __init__(self):
+        self._x = 0
+        self._y = 0
+        
+    def getX(self):
+        return self._x
+    
+    def getY(self):
+        return self._y
+    
+    def setX(self,value):
+        self._x = value
+        
+    def setY(self, value):
+        self._y = value
