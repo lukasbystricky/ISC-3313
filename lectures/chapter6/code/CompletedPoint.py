fff1b759086f5e1031c463d7a7085e2c09cf6969
Lukas Bystricky <lb13f@my.fsu.edu>
2017-07-11 09:58:05 -0400

    CompletedPoint.py: added __mul__ method

--- lectures/chapter6/code/CompletedPoint.py
+++ lectures/chapter6/code/CompletedPoint.py
@@ -17,19 +17,22 @@ class Point:
         
         self._x = x
         self._y = y
-        
+
+########################################################################
     def getX(self):
         """
         Get x
         """
         return self._x
-    
+        
+########################################################################    
     def getY(self):
         """
         Get y
         """
         return self._y
-    
+        
+########################################################################
     def setX(self,value):
         """
         Set x
@@ -39,7 +42,8 @@ class Point:
         x [float]
         """
         self._x = value
-        
+
+########################################################################
     def setY(self, value):
         """
         Set y
@@ -49,7 +53,8 @@ class Point:
         y [float]
         """
         self._y = value
-        
+
+########################################################################
     def scale(self, factor):
         """ Scales both coordinate values by a factor
         
@@ -59,7 +64,8 @@ class Point:
         """
         self._x *= factor
         self._y *= factor
-    
+
+########################################################################
     def distance(self, other):
         """
         Computes the distance between this point and another point
@@ -78,29 +84,35 @@ class Point:
 
         return np.sqrt(dx**2 + dy**2)
 
+########################################################################
     def normalize(self):
         """
         Scales the point to have unit length from origin
         """
         
-        mag = self.distance(Point()) # compute the distance from point to the origin
+        # compute the distance from point to the origin
+        mag = self.distance(Point()) 
         if mag > 0:
             # if the magnitude is not 0, scale by 1 over magnitude
             self.scale(1/mag)
-     
-    ####################################################################
+
+########################################################################
+########################################################################
     # SOLUTIONS TO IN CLASS EXERCISE
-    ####################################################################
+########################################################################
+########################################################################
     def __add__(self, other):
         """ Adds a Point to this one """
         
         return Point(self._x + other._x, self._y + other._y)
-        
+
+########################################################################
     def __sub__(self, other):
         """ Subtracts a Point from this one """
         
         return Point(self._x - other._x, self._y - other._y)
-        
+
+########################################################################
     def __neg__(self):
         """ 
         Returns the negative of this Point - i.e returns a new Point
@@ -108,7 +120,29 @@ class Point:
         """
         
         return Point(-self._x, -self._y)
+
+########################################################################
+    def __mul__(self, other):
+        """
+        Defines the multiplication operator
         
+        Parameters
+        ----------
+        other [Point/int/float]
+        
+        Returns
+        [Point/float] - depending on the type of other, returns this 
+                point multiplied by other if other is an int/float
+                returns a float giving the dot product if other is a Point
+        """
+        
+        if isinstance(other, (int,float)): # multiply by constant
+            return Point(self._x * other, self._y * other)
+        
+        elif isinstance(other, Point): # dot product
+            return self._x * other._x + self._y * other._y
+
+########################################################################
     def __abs__(self):
         """ 
         Returns the magnitude of the Point, i.e. the distance from 
@@ -116,14 +150,16 @@ class Point:
         """
         
         return self.distance(Point())
-        
+
+########################################################################
     def __eq__(self, other):
         """
         Tests if a Point is equal to this one, i.e if another point has 
         both the same x and y coordinates
         """
         return self._x == other._x and self._y == other._y
-        
+
+########################################################################
     def __str__(self):
         """
         Returns a string representation of this Point
